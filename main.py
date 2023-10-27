282a64043e30bd197061ef546349f76b399205b0
chandwi <birdfree456@protonmail.com>
2023-10-27 16:09:29 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import tkinter
+from tkinter import *
+import tkinter.messagebox
+import pickle
+
+root=Tk()
+root.title("To-Do-List")
+root.geometry("400x650+400+100")
+root.resizable(False, False)
+
+task_list = []
+
+def addTask():
+    task = todo_entry.get()
+    if task != "":
+        todolist_container.insert(tkinter.END, task)
+        todo_entry.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must enter a task.")
+
+def deleteTask():
+    try:
+        task_index = todolist_container.curselection()[0]
+        todolist_container.delete(task_index)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="You must select a task.")
+
+def saveTasks():
+    tasks = todolist_container.get(0, todolist_container.size())
+    pickle.dump(tasks, open("tasks.dat", "wb"))
+    tkinter.messagebox.showinfo(title="Saved!", message="Tasks entered are now saved.")
+
+
+def loadTasks():
+    try:
+        tasks = pickle.load(open("tasks.dat", "rb"))
+        todolist_container.delete(0, tkinter.END)
+        for task in tasks:
+            todolist_container.insert(tkinter.END, task)
+    except:
+        tkinter.messagebox.showwarning(title="Warning!", message="No tasks saved.")
+
+# Application Icon
+image_icon = PhotoImage(file="images/todolisticon.png")
+root.iconphoto(False,image_icon)
+
+# Window Background
+bg_img = root.configure(background="#FAF1ED")
+
+# Top Bar
+top_img = PhotoImage(file="images/topbar.png")
+set_topimg = Label(root, image=top_img).place(x=-53,y=10)
+todo_img = PhotoImage(file="images/todo.png")
+set_todimg = Label(root, image=todo_img).place(x=5,y=50)
+me_img = PhotoImage(file="images/me.png")
+set_meimg = Label(root, image=me_img).place(x=335,y=50)
+
+# Add task 
+frame = Frame(root, width=400, height=50, bg="white")
+frame.place(x=0, y=180)
+
+todo = StringVar()
+todo_entry = Entry(frame, width=18, font=("comic sans ms", 18), bd=0)
+todo_entry.place(x=10, y=7)
+todo_entry.focus()
+
+add_button_fnt = ("Verdana", 20, "bold")
+add_button = Button(frame, text="Add", width=5, bg="#795647", fg="white", bd=0, command=addTask)
+add_button.place(x=290, y=0)
+add_button.configure(font=add_button_fnt)
+
+# To Do List Container
+frame1 = Frame(root, bd=1, width=700, height=280,bg="#E1D5CF")
+frame1.pack(pady=(240,0))
+
+todolist_container = Listbox(frame1, font=('Comic Sans MS',15,"bold"), width=30,height=10,bg="#E1D5CF",fg="black", cursor="hand2", selectbackground="#9F516D")
+todolist_container.pack(side=LEFT, fill=BOTH, padx=2)
+
+
+scrollbar = Scrollbar(frame1)
+scrollbar.pack(side=RIGHT, fill=BOTH)
+
+todolist_container.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=todolist_container.yview)
+
+# Delete task icon
+delete_icon = PhotoImage(file="images/delete.png")
+Button(root, image=delete_icon, bd=0, command=deleteTask).place(x=90, y=550)
+delete_text = Label(root, text="Delete", font=("Comic Sans Ms", 12), bg="#FAF1ED").place(x=96, y=620)
+
+# Save task icon
+save_icon = PhotoImage(file="images/save.png")
+Button(root, image=save_icon, bd=0, command=saveTasks).place(x=170, y=550)
+save_text = Label(root, text="Save", font=("Comic Sans Ms", 12), bg="#FAF1ED").place(x=180, y=620)
+
+# Load task icon
+load_icon = PhotoImage(file="images/load.png")
+Button(root, image=load_icon, bd=0, command=loadTasks).place(x=250, y=550)
+load_text = Label(root, text="Load", font=("Comic Sans Ms", 12), bg="#FAF1ED").place(x=260, y=620)
+
+
+root.mainloop()
